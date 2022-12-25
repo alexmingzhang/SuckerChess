b64fa02401672338712f5288d59f440f9b5c330e
David K. Zhang <dzhang314@gmail.com>
2022-12-25 15:26:31 -0600

    rerun clang-format

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -3,59 +3,59 @@
 
 std::ostream &operator<<(std::ostream &os, const ChessPiece &piece) {
     switch (piece.color) {
-    case PieceColor::NONE:
-        os << ' ';
-        break;
-    case PieceColor::WHITE:
-        switch (piece.type) {
-        case PieceType::NONE:
+        case PieceColor::NONE:
             os << ' ';
             break;
-        case PieceType::KING:
-            os << "♚";
+        case PieceColor::WHITE:
+            switch (piece.type) {
+                case PieceType::NONE:
+                    os << ' ';
+                    break;
+                case PieceType::KING:
+                    os << "♚";
+                    break;
+                case PieceType::QUEEN:
+                    os << "♛";
+                    break;
+                case PieceType::ROOK:
+                    os << "♜";
+                    break;
+                case PieceType::BISHOP:
+                    os << "♝";
+                    break;
+                case PieceType::KNIGHT:
+                    os << "♞";
+                    break;
+                case PieceType::PAWN:
+                    os << "♟";
+                    break;
+            }
             break;
-        case PieceType::QUEEN:
-            os << "♛";
+        case PieceColor::BLACK:
+            switch (piece.type) {
+                case PieceType::NONE:
+                    os << ' ';
+                    break;
+                case PieceType::KING:
+                    os << "♔";
+                    break;
+                case PieceType::QUEEN:
+                    os << "♕";
+                    break;
+                case PieceType::ROOK:
+                    os << "♖";
+                    break;
+                case PieceType::BISHOP:
+                    os << "♗";
+                    break;
+                case PieceType::KNIGHT:
+                    os << "♘";
+                    break;
+                case PieceType::PAWN:
+                    os << "♙";
+                    break;
+            }
             break;
-        case PieceType::ROOK:
-            os << "♜";
-            break;
-        case PieceType::BISHOP:
-            os << "♝";
-            break;
-        case PieceType::KNIGHT:
-            os << "♞";
-            break;
-        case PieceType::PAWN:
-            os << "♟";
-            break;
-        }
-        break;
-    case PieceColor::BLACK:
-        switch (piece.type) {
-        case PieceType::NONE:
-            os << ' ';
-            break;
-        case PieceType::KING:
-            os << "♔";
-            break;
-        case PieceType::QUEEN:
-            os << "♕";
-            break;
-        case PieceType::ROOK:
-            os << "♖";
-            break;
-        case PieceType::BISHOP:
-            os << "♗";
-            break;
-        case PieceType::KNIGHT:
-            os << "♘";
-            break;
-        case PieceType::PAWN:
-            os << "♙";
-            break;
-        }
-        break;
     }
 
     return os;
