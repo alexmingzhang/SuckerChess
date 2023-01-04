bec689f2f43f0063f5e6df348e5c4d5944c8b1e0
David K. Zhang <dzhang314@gmail.com>
2023-01-04 00:33:55 -0600

    optimize insertion and checkmate testing

--- src/ChessEngine.cpp
+++ src/ChessEngine.cpp
@@ -1,6 +1,7 @@
 #include "ChessEngine.hpp"
 
-#include <utility> // for std::move
+#include <cassert> // for assert
+#include <utility> // for std::make_pair, std::move
 
 
 ChessEngineInterface::ChessEngineInterface() noexcept
@@ -11,17 +12,21 @@ ChessEngineInterface::ChessEngineInterface() noexcept
 
 const PositionInfo &ChessEngineInterface::lookup(const ChessPosition &pos
 ) noexcept {
-    if (!cache.contains(pos)) {
+    const auto location = cache.find(pos);
+    if (location != cache.end()) {
+        return location->second;
+    } else {
         std::vector<ChessMove> legal_moves;
         pos.visit_legal_moves([&](ChessMove move, const ChessPosition &) {
             legal_moves.push_back(move);
         });
         const bool in_check = pos.in_check();
-        cache.insert({
-            pos, {std::move(legal_moves), in_check}
-        });
+        const auto [iterator, inserted] = cache.insert(
+            std::make_pair(pos, PositionInfo{std::move(legal_moves), in_check})
+        );
+        assert(inserted);
+        return iterator->second;
     }
-    return cache[pos];
 }
 
 
@@ -37,8 +42,11 @@ const std::vector<ChessMove> &ChessEngineInterface::get_legal_moves() noexcept {
 
 
 bool ChessEngineInterface::checkmated(const ChessPosition &pos) noexcept {
-    const PositionInfo &info = lookup(pos);
-    return info.in_check && info.legal_moves.empty();
+    if (pos.in_check()) {
+        return get_legal_moves(pos).empty();
+    } else {
+        return false;
+    }
 }
 
 
@@ -48,8 +56,11 @@ bool ChessEngineInterface::checkmated() noexcept {
 
 
 bool ChessEngineInterface::stalemated(const ChessPosition &pos) noexcept {
-    const PositionInfo &info = lookup(pos);
-    return (!info.in_check) && info.legal_moves.empty();
+    if (pos.in_check()) {
+        return false;
+    } else {
+        return get_legal_moves(pos).empty();
+    }
 }
 
 
