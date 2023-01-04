5a6501c63360303b160f9cb2289174f37f1b7063
David K. Zhang <dzhang314@gmail.com>
2023-01-03 22:55:45 -0600

    add Preference engines

---
+++ src/Engine/Preference.cpp
@@ -0,0 +1,300 @@
+#include "Preference.hpp"
+
+#include <cassert> // for assert
+#include <utility> // for std::move
+
+#include "../Utilities.hpp"
+
+
+ChessPreference::~ChessPreference() noexcept = default;
+
+
+namespace Preference {
+
+#define DEFINE_PREFERENCE(NAME)                                                \
+    std::vector<ChessMove> NAME::pick_preferred_moves(                         \
+        [[maybe_unused]] ChessEngineInterface &interface,                      \
+        const std::vector<ChessMove> &allowed_moves                            \
+    )
+
+DEFINE_PREFERENCE(MateInOne) {
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+        return interface.checkmated(copy);
+    });
+}
+
+DEFINE_PREFERENCE(PreventMateInOne) {
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+
+        // for each possible opponent response...
+        for (ChessMove move_2 : interface.get_legal_moves(copy)) {
+            ChessPosition copy_2 = copy;
+            copy_2.make_move(move_2);
+
+            // ...ensure that response does not deliver checkmate
+            if (interface.checkmated(copy_2)) { return false; }
+        }
+        return true;
+    });
+}
+
+DEFINE_PREFERENCE(PreventDraw) {
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+        return interface.stalemated(copy) ||
+               copy.get_board().has_insufficient_material();
+    });
+}
+
+DEFINE_PREFERENCE(Check) {
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition next = interface.get_current_pos();
+        next.make_move(move);
+        return next.in_check();
+    });
+}
+
+DEFINE_PREFERENCE(Capture) {
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        return interface.get_current_pos().is_capture(move);
+    });
+}
+
+DEFINE_PREFERENCE(CaptureHanging) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessPosition &current_pos = interface.get_current_pos();
+    const ChessBoard &board = current_pos.get_board();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        return current_pos.is_capture(move) &&
+               !board.is_attacked_by(enemy, move.get_dst());
+    });
+}
+
+DEFINE_PREFERENCE(SmartCapture) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessPosition &current_pos = interface.get_current_pos();
+    const ChessBoard &board = current_pos.get_board();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        if (!current_pos.is_capture(move)) { return 0; }
+        int defenders = board.count_attacks_by(self, move.get_dst());
+        int attackers = board.count_attacks_by(enemy, move.get_dst());
+        return defenders - attackers;
+    });
+}
+
+DEFINE_PREFERENCE(Castle) {
+    const PieceColor self = interface.get_color_to_move();
+    const ChessPosition &current_pos = interface.get_current_pos();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        // always castle if possible
+        if (current_pos.is_castle(move)) { return 2; }
+
+        // determine whether this moves gives up castling rights
+        ChessPosition copy = current_pos;
+        copy.make_move(move);
+
+        // avoid giving up short castling rights
+        if (current_pos.can_short_castle(self) &&
+            !copy.can_short_castle(self)) {
+            return 0;
+        }
+
+        // avoid giving up long castling rights
+        if (current_pos.can_long_castle(self) && !copy.can_long_castle(self)) {
+            return 0;
+        }
+
+        return 1;
+    });
+}
+
+DEFINE_PREFERENCE(First) { return {*allowed_moves.begin()}; }
+
+DEFINE_PREFERENCE(Last) { return {*allowed_moves.rbegin()}; }
+
+DEFINE_PREFERENCE(Reduce) {
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+        return interface.get_legal_moves(copy).size();
+    });
+}
+
+DEFINE_PREFERENCE(Greedy) {
+    const ChessBoard &board = interface.get_current_pos().get_board();
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        const ChessPiece target = board.get_piece(move.get_dst());
+        if (target == EMPTY_SQUARE) {
+            return 7;
+        } else {
+            return static_cast<int>(target.get_type());
+        }
+    });
+}
+
+DEFINE_PREFERENCE(Swarm) {
+    const ChessSquare enemy_king_location =
+        interface.get_current_pos().get_enemy_king_location();
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        return enemy_king_location.distance(move.get_dst()) -
+               enemy_king_location.distance(move.get_src());
+    });
+}
+
+DEFINE_PREFERENCE(Huddle) {
+    const ChessSquare king_location =
+        interface.get_current_pos().get_king_location();
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        return king_location.distance(move.get_dst()) -
+               king_location.distance(move.get_src());
+    });
+}
+
+DEFINE_PREFERENCE(Sniper) {
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        return move.distance();
+    });
+}
+
+DEFINE_PREFERENCE(Sloth) {
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        return move.distance();
+    });
+}
+
+DEFINE_PREFERENCE(Conqueror) {
+    const PieceColor self = interface.get_color_to_move();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+        int result = 0;
+        for (coord_t file = 0; file < NUM_FILES; ++file) {
+            for (coord_t rank = 0; rank < NUM_RANKS; ++rank) {
+                ChessSquare square = {file, rank};
+                if (copy.get_board().is_attacked_by(self, square)) { ++result; }
+            }
+        }
+        return result;
+    });
+}
+
+DEFINE_PREFERENCE(Constrictor) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+        int result = 0;
+        for (coord_t file = 0; file < NUM_FILES; ++file) {
+            for (coord_t rank = 0; rank < NUM_RANKS; ++rank) {
+                ChessSquare square = {file, rank};
+                if (copy.get_board().is_attacked_by(enemy, square)) {
+                    ++result;
+                }
+            }
+        }
+        return result;
+    });
+}
+
+DEFINE_PREFERENCE(Reinforced) {
+    const PieceColor self = interface.get_color_to_move();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        ChessPosition copy = interface.get_current_pos();
+        copy.make_move(move);
+        return copy.get_board().is_attacked_by(self, move.get_dst());
+    });
+}
+
+DEFINE_PREFERENCE(Outpost) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessBoard &board = interface.get_current_pos().get_board();
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        return board.is_attacked_by(enemy, move.get_dst());
+    });
+}
+
+DEFINE_PREFERENCE(Gambit) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessBoard &board = interface.get_current_pos().get_board();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        return board.is_attacked_by(self, move.get_dst()) &&
+               board.is_attacked_by(enemy, move.get_dst());
+    });
+}
+
+DEFINE_PREFERENCE(Explore) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessBoard &board = interface.get_current_pos().get_board();
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        return board.is_attacked_by(self, move.get_dst()) ||
+               board.is_attacked_by(enemy, move.get_dst());
+    });
+}
+
+DEFINE_PREFERENCE(Coward) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessBoard &board = interface.get_current_pos().get_board();
+    return maximal_elements(allowed_moves, [&](ChessMove move) {
+        return board.is_attacked_by(enemy, move.get_src());
+    });
+}
+
+DEFINE_PREFERENCE(Hero) {
+    const PieceColor self = interface.get_color_to_move();
+    const PieceColor enemy = !self;
+    const ChessBoard &board = interface.get_current_pos().get_board();
+    return minimal_elements(allowed_moves, [&](ChessMove move) {
+        return board.is_attacked_by(enemy, move.get_src());
+    });
+}
+
+#undef DEFINE_PREFERENCE
+
+} // namespace Preference
+
+
+namespace Engine {
+
+
+Preference::Preference()
+    : rng(properly_seeded_random_engine())
+    , preferences() {}
+
+
+ChessMove Preference::pick_move(
+    ChessEngineInterface &interface,
+    [[maybe_unused]] const std::vector<ChessPosition> &pos_history,
+    [[maybe_unused]] const std::vector<ChessMove> &move_history
+) {
+    std::vector<ChessMove> allowed_moves = interface.get_legal_moves();
+    for (const std::unique_ptr<ChessPreference> &pref : preferences) {
+        if (allowed_moves.size() <= 1) { break; }
+        allowed_moves = pref->pick_preferred_moves(interface, allowed_moves);
+    }
+    assert(!allowed_moves.empty());
+    if (allowed_moves.size() == 1) {
+        return allowed_moves[0];
+    } else {
+        return random_choice(rng, allowed_moves);
+    }
+}
+
+
+void Preference::add_preference(std::unique_ptr<ChessPreference> &&pref) {
+    preferences.push_back(std::move(pref));
+}
+
+
+} // namespace Engine
---
+++ src/Engine/Preference.hpp
@@ -0,0 +1,115 @@
+#ifndef SUCKER_CHESS_ENGINE_PREFERENCE_HPP
+#define SUCKER_CHESS_ENGINE_PREFERENCE_HPP
+
+#include <memory>  // for std::unique_ptr, std::make_unique
+#include <random>  // for std::mt19937
+#include <utility> // for std::forward
+#include <vector>  // for std::vector
+
+#include "../ChessEngine.hpp"
+#include "../ChessMove.hpp"
+#include "../ChessPosition.hpp"
+
+
+class ChessPreference {
+
+public:
+
+    virtual ~ChessPreference() noexcept = 0;
+
+    virtual std::vector<ChessMove> pick_preferred_moves(
+        ChessEngineInterface &interface,
+        const std::vector<ChessMove> &allowed_moves
+    ) = 0;
+
+}; // class ChessPreference
+
+
+// clang-format off
+#define DECLARE_PREFERENCES(MACRO) \
+    MACRO(MateInOne,        MATE_IN_ONE,         "Ma1", "always give checkmate if possible") \
+    MACRO(PreventMateInOne, PREVENT_MATE_IN_ONE, "PM1", "avoid moving to positions that give opponent checkmate") \
+    MACRO(PreventDraw,      PREVENT_DRAW,        "PDr", "avoid stalemate and threefold repetition") \
+    MACRO(Check,            CHECK,               "Chk", "always check opponent if possible") \
+    MACRO(Capture,          CAPTURE,             "Cap", "always capture any piece if possible") \
+    MACRO(CaptureHanging,   CAPTURE_HANGING,     "CHa", "always capture hanging pieces if possible") \
+    MACRO(SmartCapture,     SMART_CAPTURE,       "SCp", "always capture pieces that have less defenders than attackers") \
+    MACRO(Castle,           CASTLE,              "Cst", "always castle if possible and avoid giving up castling rights") \
+    MACRO(First,            FIRST,               "Fst", "always make first available move") \
+    MACRO(Last,             LAST,                "Lst", "always make last available move") \
+    MACRO(Reduce,           REDUCE,              "Red", "make moves that minimize opponent's number of legal responses") \
+    MACRO(Greedy,           GREEDY,              "Grd", "make moves that maximize own material advantage") \
+    MACRO(Swarm,            SWARM,               "Swm", "move pieces toward enemy king") \
+    MACRO(Huddle,           HUDDLE,              "Hud", "move pieces toward own king") \
+    MACRO(Sniper,           SNIPER,              "Sni", "make moves that maximize distance travelled") \
+    MACRO(Sloth,            SLOTH,               "Slo", "make moves that minimize distance travelled") \
+    MACRO(Conqueror,        CONQUEROR,           "Cnq", "make moves that maximize number of squares own pieces can see") \
+    MACRO(Constrictor,      CONSTRICTOR,         "Cns", "make moves that minimize number of squares enemy pieces can see") \
+    MACRO(Reinforced,       REINFORCED,          "Rei", "always move to defended squares if possible") \
+    MACRO(Outpost,          OUTPOST,             "Out", "always move to squares that are not attacked by enemy if possible") \
+    MACRO(Gambit,           GAMBIT,              "Gam", "always move to squares that are attacked by both self and enemy if possible") \
+    MACRO(Explore,          EXPLORE,             "Exp", "always move to squares that are attacked by neither self nor enemy if possible") \
+    MACRO(Coward,           COWARD,              "Cow", "move pieces that are attacked") \
+    MACRO(Hero,             HERO,                "Hro", "move pieces that are not attacked")
+// clang-format on
+
+
+enum class PreferenceToken {
+
+#define CREATE_PREFERENCE_TOKEN(CLASS_NAME, TOKEN_NAME, STRING_NAME, COMMENT)  \
+    TOKEN_NAME,
+
+    DECLARE_PREFERENCES(CREATE_PREFERENCE_TOKEN)
+
+#undef CREATE_PREFERENCE_TOKEN
+
+}; // enum class PreferenceToken
+
+
+namespace Preference {
+
+#define CREATE_PREFERENCE_CLASS(CLASS_NAME, TOKEN_NAME, STRING_NAME, COMMENT)  \
+    class CLASS_NAME final : public ChessPreference {                          \
+        std::vector<ChessMove> pick_preferred_moves(                           \
+            ChessEngineInterface &interface,                                   \
+            const std::vector<ChessMove> &allowed_moves                        \
+        ) override;                                                            \
+    };
+
+DECLARE_PREFERENCES(CREATE_PREFERENCE_CLASS)
+
+#undef CREATE_PREFERENCE_CLASS
+
+} // namespace Preference
+
+
+namespace Engine {
+
+class Preference final : public ChessEngine {
+
+    std::mt19937 rng;
+    std::vector<std::unique_ptr<ChessPreference>> preferences;
+
+public:
+
+    Preference();
+
+    void add_preference(std::unique_ptr<ChessPreference> &&pref);
+
+    template <typename T, typename... Args>
+    void add_preference(Args &&...args) {
+        add_preference(std::make_unique<T>(std::forward<Args>(args)...));
+    }
+
+    ChessMove pick_move(
+        ChessEngineInterface &interface,
+        const std::vector<ChessPosition> &pos_history,
+        const std::vector<ChessMove> &move_history
+    ) override;
+
+}; // class Preference
+
+} // namespace Engine
+
+
+#endif // SUCKER_CHESS_ENGINE_PREFERENCE_HPP
